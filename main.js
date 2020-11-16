093cc1ae1021fa0d6773028aa9a4fe3c80be815d
chenhao <shichenhao@1911edu.com>
2020-11-16 14:44:33 +0800

    交通缴纳查询-宏柯网

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ Vue.config.productionTip = false
 Vue.prototype.$request = request
 Vue.prototype.$api = api
 Vue.prototype.$url = url
-Vue.prototype.$type = 4
+Vue.prototype.$type = 2
 
 App.mpType = 'app'
 
